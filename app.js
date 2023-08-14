b003bb853beec7a2e2dca3129e883ba98e6561d3
zevi bem <zevibem@gmail.com>
2023-08-14 16:38:52 +0300

    to commit

---
+++ app.js
@@ -0,0 +1,45 @@
+// const fs = require('fs')
+// const lodash = require('lodash') 
+// fs.readFile('data.txt', 'utf-8', function (err, data) {
+// if (err) {console.error(err)};
+
+// console.log(data);
+
+// let word = lodash.words(data)
+// console.log(word.length)
+
+// let newText = data.split(" ")
+// newText = lodash.reverse(newText)
+// newText = newText.join(" ")
+// console.log(newText)
+
+// let dabble = lodash.uniq(newText)
+// dabble = dabble.join(" ")
+// console.log(dabble)
+// console.log(dabble.length)
+
+// let word2 = lodash.words(dabble)
+// console.log(dabble)
+// console.log(word2.length)
+
+// let upper = dabble.toUpperCase()
+// console.log(upper)
+
+// let big5 = dabble.filter((five) => five.length > 5);
+// console.log(big5.join(' '))
+
+
+
+
+// for (let i = 0; i < word2.length; i++) {
+//     if (word2[i].length > 5) {
+//         console.log(word2[i])
+//     }
+//     else {
+//         continue
+//     }
+// }
+// });
+
+
+
